653f908bebaf5cd7e70edc8b7cdc224c3004e3dd
Miguel <miguel_huerres@hotmail.com>
2015-10-14 23:51:39 -0700

    segundo commit con topchef

---
+++ topchef/recetas/migrations/0001_initial.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Receta',
+            fields=[
+                ('id', models.AutoField(primary_key=True, serialize=False, auto_created=True, verbose_name='ID')),
+                ('nombre', models.CharField(max_length=30)),
+                ('instrucciones', models.TextField()),
+                ('fecha', models.DateTimeField()),
+            ],
+        ),
+    ]
